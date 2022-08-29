acff49a401542921f65dbf2232618dec8a55282e
Ellie Sargent <elliecsargent@gmail.com>
2022-08-28 20:17:03 -0500

    final commit before deploy

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     './css/styles.css',
     './js/idb.js',
     './js/index.js',
-    './manifest.webmanifest',
+    './manifest.json',
     './icons/icon-72x72.png',
     './icons/icon-96x96.png',
     './icons/icon-128x128.png',
